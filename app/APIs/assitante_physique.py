de76032628caeeb235be363022b21951f4f374ad
AymanIbnouennadre <ayman.ibnouennadre@gmail.com>
2025-04-18 16:54:23 +0100

    update marwane assitante

--- app/APIs/assitante_physique.py
+++ app/APIs/assitante_physique.py
@@ -12,18 +12,17 @@ client = openai.OpenAI(
 router = APIRouter()
 
 
-class ChatRequest(BaseModel):
-    message: str
+
 
 
 @router.post("/chat", summary="Chat with AI assistant")
-async def chat_with_assistant(request: ChatRequest):
+async def chat_with_assistant(request: str):
     try:
         response = client.chat.completions.create(
             model="llama3-8b-8192",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant supporting patients with dyslexia."},
-                {"role": "user", "content": request.message}
+                {"role": "user", "content": request}
             ]
         )
         return {"response": response.choices[0].message.content}
